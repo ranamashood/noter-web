47a6e807c04ca405671a749e21a6f6c13dbdbb51
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-02-27 22:45:58 +0500

    updated css and added animations in Sidebar component

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { NoteInterface, NotesAction, NotesActionKind } from "../../models.d";
+import Button, { StyledButton } from "../Button";
 
 interface Props {
   notes: NoteInterface[];
@@ -8,33 +10,72 @@ interface Props {
 
 const Sidebar = (props: Props) => {
   const { notes, addNote } = props;
+  const [activeButton, setActiveButton] = useState<number>(-1);
+  const [hoveredButton, setHoveredButton] = useState<number>(-1);
 
   return (
     <Wrapper>
-      <button
+      <Button
+        title="Add Note"
+        isPermanent={true}
         onClick={() => {
           addNote({
             type: NotesActionKind.ADD,
             payload: { title: "hello", details: "world" },
           });
         }}
-      >
-        Add Note
-      </button>
-      <div>
+      />
+
+      <NotesTitles onMouseLeave={() => setHoveredButton(-1)}>
+        <ButtonHover
+          hoveredButton={hoveredButton}
+          isActive={true}
+          isInvisible={true}
+          isPermanent={false}
+        >
+          hover
+        </ButtonHover>
         {notes.map((note, key) => (
-          <div key={key}>
-            {note.title} - {note.details}
-          </div>
+          <Button
+            key={key}
+            title={note.title}
+            onClick={() => setActiveButton(key)}
+            onMouseEnter={() => setHoveredButton(key)}
+            isActive={activeButton === key}
+          />
         ))}
-      </div>
+      </NotesTitles>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  background-color: red;
+  background-color: ${(props) => props.theme.colors.tertiary};
   width: 200px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+`;
+
+const NotesTitles = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  flex: 1;
+  margin: 2px 0px;
+  button {
+    margin: 2px 0px;
+  }
+`;
+
+const ButtonHover = styled(StyledButton)<{ hoveredButton: number }>`
+  position: absolute;
+  width: 100%;
+  pointer-events: none;
+  z-index: 1;
+  transform: translate(0, calc(110.5% * ${(props) => props.hoveredButton}));
+  transition: transform 300ms cubic-bezier(0.47, 0, 0.745, 0.715);
 `;
 
 export default Sidebar;
